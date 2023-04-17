4a64125b57687e816d6ac4e51e4bdfee2d0f37f2
Roger Puchol Celma <49079760+Ropuce@users.noreply.github.com>
2023-04-17 13:10:25 +0200

    USAD LA RAMA DEV, NO LA MAIN Esto esta aqui como registro de lo sucedido. No es plan de pasar archivos temporales entre PCs y complicar el tema a la hora de hacer push/pull. Este commit será sobreescrito completamente por la rama dev cuando vayamos a hacer la "version final" del prototipo (yo me encargaré de ello)

--- Project_6_Solution/Source/ModuleScene.cpp
+++ Project_6_Solution/Source/ModuleScene.cpp
@@ -27,19 +27,14 @@ bool ModuleScene::Start()
 	App->audio->PlayMusic("Assets/stage1.ogg", 1.0f);
 
 	// Colliders ---
-	App->collisions->AddCollider({ 0, 224, 3930, 16 }, Collider::Type::WALL);
 
 	// TODO 2: Add colliders for the first columns of the level
-	App->collisions->AddCollider({ 1375, 0, 111, 96 }, Collider::Type::WALL);
-	App->collisions->AddCollider({ 1375, 145, 111, 96 }, Collider::Type::WALL);
 
 	return ret;
 }
 
 update_status ModuleScene::Update()
 {
-	App->render->camera.x += 3;
-
 	return update_status::UPDATE_CONTINUE;
 }
 
