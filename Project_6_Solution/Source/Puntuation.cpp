9d17bd22c097df29518ae0c3a48b589ec9997f1f
Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>
2023-04-17 11:07:19 +0200

    Corrections to puntuation

--- Project_6_Solution/Source/Puntuation.cpp
+++ Project_6_Solution/Source/Puntuation.cpp
@@ -3,6 +3,45 @@
 
 Puntuacion::Puntuacion()
 {
-	
+    // Inicializar la puntuación en cero
+    puntuacion = 0;
+  
+
+    // Carga de la imagen de puntuación
+    // SDL_Surface* surface = 
+   /* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);*/
+
+    // Establecer la posición y dimensiones del rectángulo de la imagen de la puntuación
+    rect.x = 10;
+    rect.y = 10;
+    rect.w = 50;    // Ancho de la imagen
+    rect.h = 50;    // Alto de la imagen
+}
+
+void Puntuacion::addPuntuacion(int puntuacion)
+{
+    // Incrementar la puntuación actual por la cantidad proporcionada
+    this->puntuacion += puntuacion;
+}
+
+void Puntuacion::draw(SDL_Renderer* renderer)
+{
+    // Dibujar la imagen de la puntuación y el valor de la puntuación actual
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+
+    SDL_Color color = { 255, 255, 255 }; // Color blanco para el texto
+   
+   /* SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_FreeSurface(surface);*/
+
+    SDL_Rect text_rect;
+    text_rect.x = rect.x + rect.w + 10; // Posición x del texto a la derecha de la imagen de la puntuación
+    text_rect.y = rect.y; // Posición y del texto alineada con la imagen de la puntuación
+   // text_rect.w = surface->w; // Ancho del texto
+  //  text_rect.h = surface->h; // Alto del texto
+
+    SDL_RenderCopy(renderer, texture, NULL, &text_rect);
+    SDL_DestroyTexture(texture);
 
 }
--- Project_6_Solution/Source/Puntuation.h
+++ Project_6_Solution/Source/Puntuation.h
@@ -1,12 +1,24 @@
 #ifndef __PUNTUATION_H__
 #define __PUNTUATION_H__
 
+#include "Module.h"
+#include "Globals.h"
+
+#include "SDL/include/SDL.h"
+#pragma comment( lib, "SDL/libx86/SDL2.lib")
+#pragma comment( lib, "SDL/libx86/SDL2main.lib")
 class Puntuacion
 {
 public:
 	Puntuacion();    // default constructor declaration
 	~Puntuacion();   // destructor declaration
 	int numeration[9];
+    void addPuntuacion(int puntuacion);   // Función para agregar puntuación
+    void draw(SDL_Renderer* renderer); // Función para dibujar la puntuación en la ventana
+private:
+    int puntuacion; // Variable para almacenar la puntuación actual
+    SDL_Texture* texture;    // Textura para la imagen de la puntuación
+    SDL_Rect rect;  // Rectángulo para la imagen de la puntuación
 };
 
 #endif //__PUNTUATION_H__
