3801b83dda1539f449bf909ba105b0da8bdec353
PauMenaTorres <paumt128@gmail.com>
2023-04-30 11:18:31 +0200

    Paso de codigo para debug

--- Project_6_Solution/Source/Puntuation.cpp
+++ Project_6_Solution/Source/Puntuation.cpp
@@ -1,6 +1,7 @@
 #include "Puntuation.h"
 #include "Application.h"
 #include "ModuleFonts.h"
+#include <stdio.h>
 
 Puntuation::Puntuation(bool startEnabled)
 {
@@ -25,7 +26,7 @@ bool Puntuation::Start()
 	char specialNumbers[] = { "0123456789:PT+-" };
 	textFont = App->fonts->Load("Assets/Fonts/fonts2.png", lookupTable, 4);
 	scoreFont = App->fonts->Load("Assets/Fonts/fonts3.png", specialNumbers, 1);
-	App->fonts->BlitText(50, 50, textFont, "this is just a font test");
+	
 
 	return ret;
 	
@@ -33,12 +34,14 @@ bool Puntuation::Start()
 
 Update_Status Puntuation::Update()
 {
-	App->fonts->BlitText(50, 50, textFont, "this is just a font test");
 	return Update_Status::UPDATE_CONTINUE;
 }
 
 Update_Status Puntuation::PostUpdate()
 {
+	sprintf_s(scoreText, 10, "%7d", score);
+	App->fonts->BlitText(14, 22, textFont, scoreText);
+	App->fonts->BlitText(14, 22, textFont, "font test");
 	return Update_Status::UPDATE_CONTINUE;
 }
 
