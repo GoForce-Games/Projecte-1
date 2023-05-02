be16a3993f36aaa3d5cd7cf60dfec73d7f88712b
Hugo Castaño Cuéllar <hugocastanocuellar@gmail.com>
2023-05-02 12:12:55 +0200

    Started Intro sequence
    To do: hacer que los archivos Intro.cpp e Intro.h sean funcionales

---
+++ Project_6_Solution/Source/Intro.h
@@ -0,0 +1,23 @@
+#ifndef __INTRO_H__
+#define __INTRO_H__
+
+#include "../Module.h"
+#include "../../Utils/Animation.h"
+
+struct SDL_Texture;
+
+class Intro : public Module
+{
+public:
+	Intro(bool startEnabled);
+	~Intro();
+
+	bool Start() override;
+
+	Update_Status Update() override;
+	Update_Status PostUpdate() override;
+
+	SDL_Texture* IntroTexture = nullptr;
+};
+
+#endif
