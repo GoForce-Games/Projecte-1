0f6c67542cf54a51bbf4c6de8a7ab1bfca31c911
PauMenaTorres <117993994+PauMenaTorres@users.noreply.github.com>
2023-05-19 09:54:36 +0200

    started animation win/lose

--- Project_6_Solution/Source/ModuleWinLose.cpp
+++ Project_6_Solution/Source/ModuleWinLose.cpp
@@ -10,7 +10,23 @@
 
 WinLose::WinLose(bool startEnabled) : Module(startEnabled)
 {
+	idleAnimation.PushBack({ 399, 98, 213, 57 });
 
+	WinAnimation.PushBack({ 399, 98, 213, 57 });
+	WinAnimation.PushBack({ 400, 260, 211, 69 });
+	WinAnimation.PushBack({ 400, 424, 217, 65 });
+	WinAnimation.PushBack({ 399, 98, 213, 57 });
+	WinAnimation.PushBack({ 400, 260, 211, 69 });
+	WinAnimation.PushBack({ 400, 424, 217, 65 });
+	WinAnimation.speed = 0.02f;
+	
+	LoseAnimation.PushBack({ 50, 98, 207, 57 });
+	LoseAnimation.PushBack({ 51, 270, 212, 63 });
+	LoseAnimation.PushBack({ 51, 425, 211, 58 });
+	LoseAnimation.PushBack({ 50, 98, 207, 57 });
+	LoseAnimation.PushBack({ 51, 270, 212, 63 });
+	LoseAnimation.PushBack({ 51, 425, 211, 58 });
+	LoseAnimation.speed = 0.01f;
 }
 
 WinLose::~WinLose()
@@ -25,9 +41,15 @@ bool WinLose::Start()
 
 	bool ret = true;
 
-	WinTexture = App->textures->Load("Assets/Sprites/win_lose.png");
-	LoseTexture = App->textures->Load("Assets/Sprites/lose_win.png");
+	WinLoseTexture = App->textures->Load("Assets/Sprites/SpritesWinLose.png");
+	//WinTexture = App->textures->Load("Assets/Sprites/win_lose.png");
+	//WinTexture2 = App->textures->Load("Assets/Sprites/win_lose2.png");
+	//WinTexture3 = App->textures->Load("Assets/Sprites/win_lose3.png");
+	//LoseTexture = App->textures->Load("Assets/Sprites/lose_win.png");
+	//LoseTexture2 = App->textures->Load("Assets/Sprites/lose_win2.png");
+	//LoseTexture3 = App->textures->Load("Assets/Sprites/lose_win3.png");
 
+	currentAnimation = &idleAnimation;
 
 	return ret;
 
@@ -35,16 +57,17 @@ bool WinLose::Start()
 
 Update_Status WinLose::Update()
 {
+
 	if (gameFinish && App->puntuation->score < 1000)
 	{
 		gameFinish = false;
-		ActiveTexture = LoseTexture;
+		currentAnimation = &LoseAnimation;
 		App->audio->PlayMusic("Assets/Music/Lose.ogg", 1.0f);
 	}
 	if (gameFinish && App->puntuation->score >= 1000)
 	{
 		gameFinish = false;
-		ActiveTexture = WinTexture;
+		currentAnimation = &WinAnimation;
 		App->audio->PlayMusic("Assets/Music/Win.ogg", 1.0f);
 	}
 	if (App->input->keys[SDL_SCANCODE_F3] == KEY_DOWN)
@@ -52,35 +75,37 @@ Update_Status WinLose::Update()
 		gameFinish = true;
 
 	}
+
+	currentAnimation->Update();
+
 	return Update_Status::UPDATE_CONTINUE;
 }
 
 // Update: draw background  
 Update_Status WinLose::PostUpdate()
 {
-	if (ActiveTexture != nullptr)
-	{
-		App->render->Blit(ActiveTexture, 0, 0, NULL, false);
-		SDL_Delay(10);
-	}
+	SDL_Rect rect = currentAnimation->GetCurrentFrame();
+	App->render->Blit(WinLoseTexture, 152, 112, NULL, false);
+	SDL_Delay(10);
 
 	return Update_Status::UPDATE_CONTINUE;
 }
 bool WinLose::CleanUp()
 {
-	if (WinTexture != nullptr)
+	if (WinLoseTexture != nullptr)
 	{
-		SDL_DestroyTexture(WinTexture);
-		WinTexture = nullptr;
+		SDL_DestroyTexture(WinLoseTexture);
+		WinLoseTexture = nullptr;
 	}
-	if (LoseTexture != nullptr)
+
+	/*if (LoseTexture != nullptr)
 	{
 		SDL_DestroyTexture(LoseTexture);
 		LoseTexture = nullptr;
-	}
-	if (ActiveTexture != nullptr)
+	}*/
+	/*if (ActiveTexture != nullptr)
 	{
 		ActiveTexture = nullptr;
-	}
+	}*/
 	return true;
 }
--- Project_6_Solution/Source/ModuleWinLose.h
+++ Project_6_Solution/Source/ModuleWinLose.h
@@ -1,5 +1,6 @@
 #include "Module.h"
 #include "Globals.h"
+#include "Animation.h"
 
 struct SDL_Texture;
 
@@ -33,7 +34,20 @@ public:
     bool gameFinish = false;
     
     // The scene sprite sheet loaded into an SDL_Texture
+
+    SDL_Texture* WinLoseTexture = nullptr;
     SDL_Texture* WinTexture = nullptr;
+    SDL_Texture* WinTexture2 = nullptr;
+    SDL_Texture* WinTexture3 = nullptr;
     SDL_Texture* LoseTexture = nullptr;
+    SDL_Texture* LoseTexture2 = nullptr;
+    SDL_Texture* LoseTexture3 = nullptr;
     SDL_Texture* ActiveTexture = nullptr;
+
+
+    Animation* currentAnimation = nullptr;
+    Animation idleAnimation;
+    Animation WinAnimation;
+    Animation LoseAnimation;
+
 };
