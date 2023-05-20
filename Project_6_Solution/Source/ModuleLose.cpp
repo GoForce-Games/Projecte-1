0a73e17665bd1bbf00284833bc6a0a79ce046fb0
PauMenaTorres <paumt128@gmail.com>
2023-05-20 12:41:36 +0200

    update modulelose.cpp

--- Project_6_Solution/Source/ModuleLose.cpp
+++ Project_6_Solution/Source/ModuleLose.cpp
@@ -20,28 +20,28 @@ ModuleLose::~ModuleLose()
 {}
 
 bool ModuleLose::Start() {
-	/*LOG("Loading gameover assets");
+	LOG("Loading gameover assets");
 
 	bool ret = true;
 	textFont = App->puntuation->textFont;
 	LoseTexture = App->textures->Load("Assets/Sprites/gameover.png");
 	App->audio->PlayMusic("Assets/Music/Game Over.ogg", 1.0f);
 	App->render->camera.x = 0;
-	App->render->camera.y = 0;*/
+	App->render->camera.y = 0;
 	return true;
 }
 Update_Status ModuleLose::Update() 
-{/*
+{
 	GamePad& pad = App->input->pads[0];
 	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN || pad.a)
 	{
 		App->fade->FadeToBlack((Module*)App->lose_screen, (Module*)App->intro, 90);
-	}*/
+	}
 	return Update_Status::UPDATE_CONTINUE;
 }
 Update_Status ModuleLose::PostUpdate()
 {
-	/*App->render->Blit(LoseTexture, 0, 0, NULL);*/
+	App->render->Blit(LoseTexture, 0, 0, NULL);
 	return Update_Status::UPDATE_CONTINUE;
 }
 bool ModuleLose::CleanUp() 
