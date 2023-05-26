d91a3a349e34354c39ff44e1fad3385553e02d9a
Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>
2023-05-26 08:24:39 +0200

    60fps Only

--- Project_6_Solution/Source/Main.cpp
+++ Project_6_Solution/Source/Main.cpp
@@ -6,84 +6,94 @@
 #pragma comment( lib, "SDL/libx86/SDL2.lib")
 #pragma comment( lib, "SDL/libx86/SDL2main.lib")
 
+const int FPS = 60;
+const int FRAME_DELAY = 1000 / FPS;
 
 enum class main_states
 {
-	MAIN_CREATION,
-	MAIN_START,
-	MAIN_UPDATE,
-	MAIN_FINISH,
-	MAIN_EXIT
+    MAIN_CREATION,
+    MAIN_START,
+    MAIN_UPDATE,
+    MAIN_FINISH,
+    MAIN_EXIT
 };
 
 Application* App = nullptr;
 
 int main(int argc, char* argv[])
 {
-	ReportMemoryLeaks();
+    ReportMemoryLeaks();
 
-	int main_return = EXIT_FAILURE;
-	main_states state = main_states::MAIN_CREATION;
+    int main_return = EXIT_FAILURE;
+    main_states state = main_states::MAIN_CREATION;
 
-	while (state != main_states::MAIN_EXIT)
-	{
-		switch (state)
-		{
-			case main_states::MAIN_CREATION:
-			{
-				LOG("Application Creation --------------\n");
-				App = new Application();
-				state = main_states::MAIN_START;
-			}	break;
+    while (state != main_states::MAIN_EXIT)
+    {
+        switch (state)
+        {
+        case main_states::MAIN_CREATION:
+        {
+            LOG("Application Creation --------------\n");
+            App = new Application();
+            state = main_states::MAIN_START;
+        }   break;
 
-			case main_states::MAIN_START:
-			{
-				LOG("Application Start --------------\n");
-				if(App->Init() == false)
-				{
-					LOG("Application Init exits with error -----\n");
-					state = main_states::MAIN_EXIT;
-				}
-				else
-				{
-					state = main_states::MAIN_UPDATE;
-				}
-			}	break;
+        case main_states::MAIN_START:
+        {
+            LOG("Application Start --------------\n");
+            if (App->Init() == false)
+            {
+                LOG("Application Init exits with error -----\n");
+                state = main_states::MAIN_EXIT;
+            }
+            else
+            {
+                state = main_states::MAIN_UPDATE;
+            }
+        }   break;
 
-			case main_states::MAIN_UPDATE:
-			{
-				Update_Status status = App->Update();
+        case main_states::MAIN_UPDATE:
+        {
+            Uint32 frameStart = SDL_GetTicks();
 
-				if (status == Update_Status::UPDATE_ERROR)
-				{
-					LOG("Application Update exits with error -----\n");
-					state = main_states::MAIN_EXIT;
-				}
-				else if (status == Update_Status::UPDATE_STOP)
-				{
-					state = main_states::MAIN_FINISH;
-				}
-			}	break;
+            Update_Status status = App->Update();
 
-			case main_states::MAIN_FINISH:
-			{
-				LOG("Application Finish --------------\n");
-				if (App->CleanUp() == true)
-				{
-					main_return = EXIT_SUCCESS;
-				}
-				else
-				{
-					LOG("Application CleanUp exits with error -----\n");
-				}
-				state = main_states::MAIN_EXIT;
-			}
-		}
-	}
+            if (status == Update_Status::UPDATE_ERROR)
+            {
+                LOG("Application Update exits with error -----\n");
+                state = main_states::MAIN_EXIT;
+            }
+            else if (status == Update_Status::UPDATE_STOP)
+            {
+                state = main_states::MAIN_FINISH;
+            }
 
-	LOG("\nBye :)\n");
+            Uint32 frameTime = SDL_GetTicks() - frameStart;
+            if (frameTime < FRAME_DELAY)
+            {
+                SDL_Delay(FRAME_DELAY - frameTime);
+            }
+        }   break;
 
-	delete App;
+        case main_states::MAIN_FINISH:
+        {
+            LOG("Application Finish --------------\n");
+            if (App->CleanUp() == true)
+            {
+                main_return = EXIT_SUCCESS;
+            }
+            else
+            {
+                LOG("Application CleanUp exits with error -----\n");
+            }
+            state = main_states::MAIN_EXIT;
+        }
+        }
+    }
 
-	return main_return;
+    LOG("\nBye :)\n");
+
+    delete App;
+
+    return main_return;
 }
